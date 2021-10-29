5fdaaa9d8dc508775dc6a0a867fe92adcb175475
VasudevaraoReddy <87357441+VasudevaraoReddy@users.noreply.github.com>
2021-10-29 22:22:05 +0530

    Update index.js

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,6 +1,7 @@
 import { Redirect } from "react-router-dom";
 import React, { useContext, useState, useEffect } from "react";
 import { store } from "../../App";
+import Loader from 'react-loader-spinner'
 import Axios from "axios";
 import "./index.css";
 
@@ -16,23 +17,25 @@ const Profile = () => {
         "x-token": token,
       },
     })
-      .then((res) => setData(res.data))
+      .then((res) => {
+        setData(res.data)
+      })
       .catch((err) => console.log(err));
   }, []);
   if (token === null) {
     return <Redirect to="/login" />;
   }
   return (
-    <>
-      {data && (
+    <div className="container">
+      {data ? (
         <div className="profile">
           <h1>Welcome {data[0].username}</h1>
           <button type="button" onClick={onClickLogout} className="btn">
             Logout
           </button>
         </div>
-      )}
-    </>
+      ) : <Loader type="Oval" color="blue" height={50} width={50} />}
+    </div>
   );
 };
 
